c5d1f2802e690a801f0d2d9ba120fc5f824d1280
ShuheiOhno <inu.suika8@gmail.com>
2020-08-27 15:18:18 +0900

    project(モデル、ビュー)
    新規作成、編集、削除、詳細ページ完了

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    project = Project.find(params[:id])
+    project.update(project_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    project = Project.find(params[:id])
+    project.destroy
+    redirect_to root_path
+  end
+
+private
+  def project_params
+    params.require(:project).permit(
+      :title, :explanation, :deadline
+    ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_053927) do
+ActiveRecord::Schema.define(version: 2020_08_27_054610) do
+
+  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "explanation"
+    t.datetime "deadline"
+    t.bigint "user_id"
+    t.integer "complete_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_08_27_053927) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
