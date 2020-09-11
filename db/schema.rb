ddf60ad1457b5948c133deb501c61ccd7db75605
ShuheiOhno <inu.suika8@gmail.com>
2020-09-12 04:36:57 +0900

    コメント機能（投稿のみ）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_000537) do
+ActiveRecord::Schema.define(version: 2020_09_11_070255) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.string "image"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "join_project_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
@@ -116,6 +125,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_000537) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "join_project_users", "projects"
   add_foreign_key "join_project_users", "users"
   add_foreign_key "project_comments", "projects"
