fdd6a6933a493fee4c0975d2d78b1ce522b167b8
ShuheiOhno <inu.suika8@gmail.com>
2020-09-18 10:56:23 +0900

    modify logout link
    #What
    ログイン、ログアウト後のリダイレクト先を修正
    devise画面レイアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_path(id: current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+  #     user_params.permit(:name, :email)
+  #   end
+  # end
 end
